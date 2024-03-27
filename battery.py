9f8a07bf665aad2f751bd066c9b0a06f8c6c5713
Watoc Masc <oushen.m4@gmail.com>
2024-03-27 13:40:47 +0200

    update battery

--- battery.py
+++ battery.py
@@ -7,14 +7,20 @@ def show():
         battery = []
         for bat in file:
             battery.append(bat)
-    for i in battery[0]:
-        if i == '%':
-            inx = battery[0].index(i)
-            battery1 = battery[0][inx-2]+battery[0][inx-1]
-    for i in battery[1]:
-        if i == '%':
-            inx = battery[1].index(i)
-            battery2 = battery[1][inx-2]+battery[1][inx-1]
+    try:        
+        for i in battery[0]:
+            if i == '%':
+                inx = battery[0].index(i)
+                battery1 = battery[0][inx-2]+battery[0][inx-1]
+    except:
+        battery1 = '0'
+    try:
+        for i in battery[1]:
+            if i == '%':
+                inx = battery[1].index(i)
+                battery2 = battery[1][inx-2]+battery[1][inx-1]
+    except:
+        battery2 = '0'
     os.remove('battery_temp.txt')
     print(f'{(int(battery1.strip())+int(battery2.strip()))//2}%')
 
